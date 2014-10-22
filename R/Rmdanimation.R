35f59fba3d8312f761afa5caa69a978a4996e584
Kevin <kevin.stadler@ed.ac.uk>
2014-10-22 22:15:18 +0100

    Adding doc and examples

--- R/Rmdanimation.R
+++ R/Rmdanimation.R
@@ -2,18 +2,24 @@
 #' @name Rmdanimation
 #' @importFrom animation ani.options saveGIF saveHTML
 
-ani.options(autobrowse=FALSE, autoplay=FALSE, imgdir="animations")
+ani.options(autobrowse=FALSE, autoplay=FALSE, imgdir="animations/")
 
 #' Create an animated gif to be used for embedding in a Markdown file
 #'
+#' @param expr an R expression to be evaluated to create a sequence of images
+#' @param filename unique filename to be used for the saved image file (without extension)
+#' @param title animation title, used as Alt text
+#' @param allowHTML logical: if TRUE (the default), decorate the return value with some HTML to allow basic navigation/restarting of the animation on click
+#' @param ... other arguments passed to \code{\link{ani.options}}
 #' @return a Markdown/HTML snippet that can be used to embed the generated animation
+#' @seealso \code{\link{saveGIF}}
 #' @export
 animatedGIF <- function(expr, filename, title=filename, allowHTML=TRUE, ...) {
-  filename <- paste(ani.options("imgdir"), filename, ".gif", sep="")
+  filename <- paste(filename, ".gif", sep="")
   if (file.exists(filename))
     file.remove(filename)
-  saveGIF(expr, movie.name=filename, title=title, ...)
-  mdstring <- paste("![", title, "](", ani.options("imgdir"), filename, ")", sep="")
+  suppressMessages(saveGIF(expr, movie.name=filename, title=title, ...))
+  mdstring <- paste("![", title, "](", filename, ")", sep="")
   if (allowHTML) {
     # add some limited navigatability to the gif by resetting the animation on click
     paste('<a onclick="this.children[0].src=this.children[0].src;">', mdstring, '</a>', sep='')
@@ -22,12 +28,16 @@ animatedGIF <- function(expr, filename, title=filename, allowHTML=TRUE, ...) {
   }
 }
 
-#' Creates an HTML file for displaying and navigating an animation
+#' Create an HTML file for displaying and navigating an animation
 #'
+#' @param expr an R expression to be evaluated to create a sequence of images
+#' @param filename unique filename to be used for the saved image files (without extension)
+#' @param title animation title, used for the animation page as well as Alt text for the Iframe
+#' @param ... other arguments passed to \code{\link{ani.options}}
 #' @return an HTML-Iframe snippet that can be used to embed the generated animation
+#' @seealso \code{\link{saveHTML}}
 #' @export
 animatedHTML <- function(expr, filename, title=filename, ani.width=ani.options("ani.width"), ani.height=ani.options("ani.height"), ...) {
-  saveHTML(expr, img.name=filename, htmlfile=paste(filename, ".html", sep=""), ani.width=ani.width, ani.height=ani.height, ...) # single.opts=
-  paste('<iframe src="', filename, '.html" width="', ani.width, '" height="', ani.height, '">Animation &quot;', title, "&quot;</iframe>", sep="")
+  suppressMessages(saveHTML(expr, img.name=filename, htmlfile=paste(filename, ".html", sep=""), verbose=FALSE, ani.width=ani.width, ani.height=ani.height, ...)) # single.opts=
+  paste('<iframe src="', filename, '.html" width="', ani.width+150, '" height="', ani.height+150, '">Animation &quot;', title, "&quot;</iframe>", sep="")
 }
-
--- README.md
+++ README.md
@@ -10,9 +10,28 @@ It currently consists of two basic wrapper functions around R's [animation](http
 
 ## Usage
 
-Since the functions are just a thin wrapper around the *animation* package, all [ani.options](http://cran.r-project.org/web/packages/animation/animation.pdf#ani.options) can be passed as extra arguments to control animation creation.
+Since the functions are just a thin wrapper around the *animation* package, all [ani.options](http://cran.r-project.org/web/packages/animation/animation.pdf#ani.options) can be passed as extra arguments to control the details of the animation. The animations are saved to external files and the functions return a Markdown/HTML snippet that, when printed to the output, embed the animation in your document. There are two ways to embed animations:
 
-    ````{r someanimation, echo=F, display='asis'}
+1. in a normal R code chunk, use the *results='asis'* option and call *cat()* to print the Markdown snippet (otherwise you'll end up with superfluous line numbering added by *print()*). This way multiple animations can be output by one code chunk.
+
+    ````{r someanimations, results='asis'}
     library(Rmdanimation)
-    cat(animatedGIF({for (i in 0:10) plot(i, i, xlim=c(0,10), ylim=c(0,10))}, "myfirstanimation", interval=0.1))
+    cat(animatedGIF({for (i in 0:10) plot(i, i, xlim=c(0,10), ylim=c(0,10))}, "myfirstanimation"))
+    
+    # the second animation stop after playing through it once
+    cat(animatedGIF({for (i in 0:10) plot(i, i, xlim=c(0,10), ylim=c(0,10))}, "stoppinganimation", loop=1))
     ````
+
+2. alternatively, if you don't want your code to be printed anyway, you can simply use the inline code chunk syntax:
+
+    `r library(Rmdanimation); animatedGIF({for (i in 0:10) plot(i, i, xlim=c(0,10), ylim=c(0,10))}, "myfirstanimation")`
+
+## Navigation
+
+Only *animatedGIF()* called with *allowHTML=FALSE* will return a pure Markdown image snippet. Any other call will also return some HTML decoration to aid in controlling the animation when embedded in a website, which is particularly useful if you want semi-interactive animations in an [Rmarkdown-based reveal.js slideshow](https://github.com/jjallaire/revealjs) or the like.
+
+*animatedHTML()* in particular will create a separate HTML file with your animation using [SciAnimator](https://github.com/brentertz/scianimator) which comes with fully configurable frame-by-frame navigation tools. The HTML snippet returned by the function is simply an Iframe displaying this page.
+
+    `r animatedHTML({for (i in 0:10) plot(i, i, xlim=c(0,10), ylim=c(0,10))}, "navigableanimation")`
+
+See [revealjs.Rmd](examples/revealjs.Rmd) for an example using animations in a reveal.js slideshow.
